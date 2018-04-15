4149cda9972c52653a58083e7c0e69e2f4169fd6
Jeff Currier <Jeff@Jeffs-MacBook-Pro.local>
2018-04-15 15:46:47 -0700

    Hooking up user classes

--- src/main/java/com/amazon/atlas/resources/UsersResource.java
+++ src/main/java/com/amazon/atlas/resources/UsersResource.java
@@ -1,33 +1,51 @@
 package com.amazon.atlas.resources;
 
 import com.amazon.atlas.data.User;
+import com.amazon.atlas.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 public class UsersResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(UsersResource.class);
+    UserService mInstance = UserService.instance();
 
     @GET
     @Path("{id}")
     public Response getUser(@PathParam("id") String userId) {
-        LOGGER.info("fetch user request - id = {%s}", userId);
+        LOGGER.info("fetch user request for {%s}", userId);
 
-        return Response.ok("", MediaType.APPLICATION_JSON).build();
+        Optional<User> foundUser = mInstance.find(userId);
+        if(!foundUser.isPresent()) {
+            LOGGER.info("user id {s} - not found", userId);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        LOGGER.info("Found user {s}; returning it", userId);
+        return Response.ok(foundUser.get(), MediaType.APPLICATION_JSON).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createUser(User newUser) {
-
         LOGGER.info("create user request for {%s}", newUser.getEmail());
 
-        return Response.ok("", MediaType.APPLICATION_JSON).build();
+        Optional<User> user = mInstance.create(newUser.getId(), newUser.getEmail(), newUser.getPassword(),
+                newUser.getIsAdmin());
+
+        if(!user.isPresent()) {
+            LOGGER.error("User creation failed; returning 500");
+            return Response.serverError().build();
+        }
+
+        LOGGER.info("User {s} created!", newUser.getId());
+        return Response.ok(user.get(), MediaType.APPLICATION_JSON).build();
     }
 
     @PUT
@@ -38,8 +56,13 @@ public class UsersResource {
         LOGGER.info("update user request for {%s}", userId);
 
         User updatedUser = null;
+        boolean result = mInstance.update(userId, user.getPassword(), user.getIsAdmin());
+        if(!result) {
+            Response.serverError().build();
+        }
 
-        return Response.ok("", MediaType.APPLICATION_JSON).build();
+        LOGGER.info("returning updated user {s}", userId);
+        return getUser(userId);
     }
 
     @DELETE
@@ -48,6 +71,13 @@ public class UsersResource {
 
         LOGGER.info("delete user request for {%s}", userId);
 
+        boolean result = mInstance.delete(userId);
+        if(!result) {
+            LOGGER.error("Failed to delete user {s}", userId);
+            Response.serverError().build();
+        }
+
+        LOGGER.info("User {s} removed", userId);
         return Response.ok("", MediaType.APPLICATION_JSON).build();
     }
 }
--- src/main/java/com/amazon/atlas/service/UserService.java
+++ src/main/java/com/amazon/atlas/service/UserService.java
@@ -1,10 +1,32 @@
 package com.amazon.atlas.service;
 
 import com.amazon.atlas.data.User;
+import com.amazon.atlas.util.ClientHelper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.DeleteItemResult;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 
 public class UserService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+    private final String USER_TABLE_NAME = "Users";
+    private DynamoDB m_dynamo = null;
     private static UserService mInstance = null;
+    private Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static synchronized  UserService instance() {
         if(mInstance == null) {
@@ -14,30 +36,132 @@ public class UserService {
         return mInstance;
     }
 
-    public User createUser(String emailAddress, String password, boolean isAdmin) {
+    public Optional<User> create(String userId, String emailAddress, String password, boolean isAdmin) {
+        assert userId != null;
         assert emailAddress != null;
         assert password != null;
+        Optional<User> newUser;
 
-        User newUser = new User(emailAddress, password, isAdmin);
+        LOGGER.info("Creating user ({s}, {s}, {s}, {b}", userId, emailAddress, password, isAdmin);
 
+        newUser = Optional.of(new User(userId, emailAddress, password, new Date(), isAdmin));
+
+        try {
+            Table userTable = m_dynamo.getTable(USER_TABLE_NAME);
+            Item userItem = new Item()
+                    .withPrimaryKey("Id", userId)
+                    .withString("EmailAddress", emailAddress)
+                    .withString("Password", password)
+                    .withBoolean("IsAdmin", isAdmin)
+                    .withString("LastUpdated", formatter.format(newUser.get().getLastUpdated()));
+
+            userTable.putItem(userItem);
+        } catch (Exception ex) {
+            LOGGER.error("Caught exception committing user [{s}] to DynamoDB", emailAddress, ex);
+            newUser.empty();
+            return newUser;
+        }
+
+        LOGGER.info("User [{s}] created", emailAddress);
         return newUser;
     }
 
-    public boolean changePassword(User user, String oldPassword, String newPassword) {
+    public boolean update(String userId, String newPassword, boolean isAdmin) {
+        assert userId != null;
+        assert newPassword != null;
+
+        LOGGER.info("Updating User {s} - pw={s} isAdmin={b}", userId, newPassword, isAdmin);
+        try {
+            Table userTable = m_dynamo.getTable(USER_TABLE_NAME);
+
+            UpdateItemSpec updateItemSpec = null;
+            UpdateItemOutcome outcome = null;
+
+            updateItemSpec = new UpdateItemSpec()
+                    .withPrimaryKey("Id", userId)
+                    .withUpdateExpression("set Id = :user_id, Password = :pw, IsAdmin = :is_admin")
+                    .withValueMap(new ValueMap()
+                            .withString(":user_id", userId)
+                            .withString(":pw", newPassword)
+                            .withBoolean("is_admin", isAdmin)
+                    )
+                    .withReturnValues(ReturnValue.ALL_OLD);
+
+            outcome = userTable.updateItem(updateItemSpec);
+        }catch (ConditionalCheckFailedException ex) {
+            if (ex.getErrorCode().equals("ConditionalCheckFailedException")) {
+                LOGGER.info("User {s} could not be found to be updated", userId);
+            }
+
+            LOGGER.error("User {s} update failed [check failure]", userId, ex);
+            return false;
+        }catch (Exception ex) {
+            LOGGER.error("User {s} update failed", userId, ex);
+
+            return false;
+        }
+
+        LOGGER.info("User {s} - updated", userId);
         return true;
     }
 
-    public User find(String emailAddress) {
-        assert emailAddress != null;
+    public Optional<User> find(String userId) {
+        assert userId != null;
+        Optional<User> foundUser;
 
-        return null;
+        LOGGER.info("Finding user id {s}", userId);
+        try {
+            Table userTable = m_dynamo.getTable(USER_TABLE_NAME);
+
+            Item item = userTable.getItem("Id", userId,
+                    "UserId, EmailAddress, Password, IsAdmin", null);
+
+            if(item != null) {
+                ObjectMapper mapper = new ObjectMapper();
+                String s = item.toJSONPretty();
+                foundUser = Optional.of(mapper.readValue(item.toJSON(), User.class));
+            } else {
+
+                LOGGER.info("NULL user item returned");
+                foundUser = Optional.empty();
+            }
+
+        } catch (Exception ex) {
+            LOGGER.error("Caught exception while finding user {s}", userId, ex);
+            foundUser = Optional.empty();
+        }
+
+        return foundUser;
     }
 
-    public boolean delete(User user) {
-       assert user != null;
+    public boolean delete(String userId) {
+       assert userId != null;
 
-       return true;
+        LOGGER.info("Deleting user ({s})", userId);
+
+        DeleteItemOutcome result = null;
+        try {
+            Table userTable = m_dynamo.getTable(USER_TABLE_NAME);
+
+            DeleteItemSpec deleteItemSpec = new DeleteItemSpec()
+                    .withPrimaryKey("Id", userId)
+                    .withConditionExpression("Id = :val")
+                    .withValueMap(new ValueMap()
+                            .withString(":val", userId))
+                    .withReturnValues(ReturnValue.ALL_OLD);
+
+            result = userTable.deleteItem(deleteItemSpec);
+        } catch (Exception ex) {
+            LOGGER.error("Caught exception removing user [{s}] to DynamoDB", userId, ex);
+            return false;
+        }
+
+        LOGGER.info("User [{s}] deleted", userId);
+        return true;
     }
 
-    private UserService() { }
+    private UserService() {
+        AmazonDynamoDBClient client = ClientHelper.instance().getDynamoClient();
+        m_dynamo = new DynamoDB(client);
+    }
 }
